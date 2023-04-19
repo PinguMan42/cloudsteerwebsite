89dd115ee6811641817ef65a215e6a3c916bd300
PinguMan42 <122866026+PinguMan42@users.noreply.github.com>
2023-04-19 15:33:53 +0530

    First Build of the website

---
+++ data.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+function loadData() {
+	var productName1:PropertyStrength;
+	var productDesc1:;
+	var productName2:PropertyAble;
+	var productDesc2:;
+	var productName3:CloudEngage;
+	var productDesc3:;
+	var productName4:PropGPT;
+	var productDesc4:;
+	
+}
